689a4f6ce4395fb1f06ec96de19a16fc73a8b645
Yudi Purwanto <purwantoyudi42@gmail.com>
2014-12-18 12:04:00 +0700

    Update ga_api.php

--- application/config/ga_api.php
+++ application/config/ga_api.php
@@ -1,8 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['profile_id']	= 'ga:85277414'; // GA profile id
-$config['email']		= 'purwantoyudi42@gmail.com'; // GA Account mail
-$config['password']		= 'jailbodyinside15'; // GA Account password
+$config['email']		= ' '; // GA Account mail
+$config['password']		= ' '; // GA Account password
 
 $config['cache_data']	= true; // request will be cached
 $config['cache_folder']	= FCPATH.'/files/ga_files/'; // read/write
